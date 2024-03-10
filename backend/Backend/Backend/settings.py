bf7fe445a4827f04bf09ed8f50b196228a55412e
Victor Medeiros <victor.medeiros@partner.bmw.com.br>
2024-03-10 20:16:09 -0300

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   backend/Backend/Backend/settings.py

--- backend/Backend/Backend/settings.py
+++ backend/Backend/Backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-%=-7pr$rzwq$$fkc9vc#eok#hqb$j@rrg5%8%$_@@cqa*$rw-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
