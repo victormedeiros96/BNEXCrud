2efd10d6036d494f1b1be827a393a5b6977ed3da
Victor Medeiros <victor.medeiros@partner.bmw.com.br>
2024-03-10 17:32:31 -0300

    remove debug mode

--- backend/Backend/Backend/settings.py
+++ backend/Backend/Backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%=-7pr$rzwq$$fkc9vc#eok#hqb$j@rrg5%8%$_@@cqa*$rw-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
