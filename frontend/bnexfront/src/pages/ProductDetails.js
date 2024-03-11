0512a292a16ce37e317a7d97baa3484a4b9c94bf
Victor Medeiros <victor.medeiros@partner.bmw.com.br>
2024-03-11 02:14:03 -0300

    Add the option to delete some product.

--- frontend/bnexfront/src/pages/ProductDetails.js
+++ frontend/bnexfront/src/pages/ProductDetails.js
@@ -6,7 +6,27 @@ const ProductDetails = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const navigate = useNavigate();
+  const handleDelete = () => {
+    if (window.confirm('Tem certeza que deseja remover este produto?')) {
+      const token = localStorage.getItem('accessToken');
+      const API_BASE = process.env.REACT_APP_API_URL;
+      const API_URL = `${API_BASE}:8000/produtos/${id}/`;
 
+      fetch(API_URL, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao remover o produto');
+        }
+        navigate('/products');
+      })
+      .catch(error => console.error('Error removing product: ', error));
+    }
+  };
   useEffect(() => {
     const token = localStorage.getItem('accessToken');
     fetch(`${API_URL}${id}/`, {
@@ -37,6 +57,7 @@ const ProductDetails = () => {
           <p>{product.descricao}</p>
           <p className="grey-text">R$ {product.valor}</p>
           <button className="btn" onClick={() => navigate(`/edit/${id}`)}>Editar</button>
+          <button className="btn" onClick={handleDelete}>Deletar</button>
         </div>
       </div>
     </div>
