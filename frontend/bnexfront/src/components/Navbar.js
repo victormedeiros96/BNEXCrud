6c21e19bb5e5675a05c193340e695f73b508c40e
Victor Medeiros <victor.medeiros@partner.bmw.com.br>
2024-03-11 02:32:40 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   frontend/bnexfront/public/index.html
    	modified:   frontend/bnexfront/src/components/Navbar.js

--- frontend/bnexfront/src/components/Navbar.js
+++ frontend/bnexfront/src/components/Navbar.js
@@ -13,22 +13,27 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="nav-wrapper ">
-        <Link to="/" className="brand-logo left hide-on-med-and-down">BNEX Products</Link>
-        <ul id="nav-mobile" className="right">
-          {isAuthenticated ? (
-            <>
-              <li><Link to="/products">Produtos</Link></li>
-              <li><Link to="/create">Adicionar Produto</Link></li>
-              <li><a href="#!" onClick={handleLogout}>Logout</a></li>
-            </>
-          ) : (
-            <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/register">Registrar</Link></li>
-            </>
-          )}
-        </ul>
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col s3 offset-s1">
+          <Link to="/" className="brand-logo left hide-on-med-and-down">BNEX Products</Link>
+          </div>
+          <div className="col s8"></div>
+          <ul id="nav-mobile" className="right">
+            {isAuthenticated ? (
+              <>
+                <li><Link to="/products">Produtos</Link></li>
+                <li><Link to="/create">Adicionar Produto</Link></li>
+                <li><a href="#!" onClick={handleLogout}>Logout</a></li>
+              </>
+            ) : (
+              <>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Registrar</Link></li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
